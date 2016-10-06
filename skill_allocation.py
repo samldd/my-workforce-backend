024a48d9fdf53af31cfe3da76098b09b89888b6d
sam <sam.landuydt>
2016-10-06 13:50:48 +0200

    final version

--- skill_allocation.py
+++ skill_allocation.py
@@ -1,5 +1,5 @@
-from flask import jsonify
-
+from flask import jsonify, request
+from prefixes import prefixes
 
 # get employee suggestions
 
@@ -7,20 +7,17 @@ from flask import jsonify
 @app.route('/employee_sugestions/')
 def employee_sugestions():
     helpers.log("employee sugestions")
-    q = """
-        prefix foaf: <http://xmlns.com/foaf/spec/>
-        prefix mu:  <http://mu.semte.ch/vocabularies/core/>
-        prefix skos: <http://www.w3.org/2004/02/skos/core#>
-        prefix default: <http://example.org/MyCompany/>
-
-        SELECT distinct ?name ?uuid
-        WHERE{
-          ?skill mu:uuid "ccd0a1d9-afda-43d9-b901-96344886e14d".
-          ?skill skos:prefLabel ?l.
-          ?employee default:hasSkill/esco:hasSkill ?skill.
-          ?employee foaf:name ?name; mu:uuid ?uuid.
-        }
-    """
+    skillId = request.args.get("id")
+    helpers.log(skillId)
+    q = prefixes + """
+    SELECT distinct ?name ?uuid
+    WHERE{
+        ?skill mu:uuid "%s".
+        ?skill skos:prefLabel ?l.
+        ?employee default:hasSkill/esco:hasSkill ?skill.
+        ?employee foaf:name ?name; mu:uuid ?uuid.
+    }
+        """ % skillId
 
     result = helpers.query(q)
     bindings = result["results"]["bindings"]
--- web.py
+++ web.py
@@ -1,46 +1,30 @@
-import datetime
-import json
+from datetime import datetime
 from flask import request, jsonify
 from timeline import *
 from improvement import *
 from skill_allocation import *
-
-def get_employees():
-    q =  " prefix foaf:   <http://xmlns.com/foaf/spec/> "
-    q += " SELECT distinct (str(?n) as ?employeeNames) "
-    q += " WHERE{"
-    q += "   GRAPH <http://localhost:8890/MyWorkForce> {"
-    q += "     ?p foaf:name ?n."
-    q += "   }"
-    q += " }"
-    res = helpers.query(q)
-    data = json.dumps(res)
-    return data
+from prefixes import prefixes
 
 @app.route('/timeline/')
 def get_skill_timeline():
     """Selects all employees with the given skill that are active at the company."""
-    today = datetime.datetime.today().date()
+    today = datetime.today().date()
     skillURI = str("http://data.europa.eu/esco/skill/" + request.args.get("skillid"))
 
-    q =  """ prefix esco: <http://data.europa.eu/esco/model#>
-             prefix skosxl: <http://www.w3.org/2008/05/skos-xl#>
-             prefix default: <http://example.org/MyCompany/>
-             prefix foaf: <http://xmlns.com/foaf/spec/>
-
-             SELECT distinct ?name ?sDate ?aDate ?eDate
-             WHERE{
-                 GRAPH <http://mu.semte.ch/application>{
-                     ?employee default:hasSkill ?skillemp.
-                     ?employee foaf:name ?name.
-                     ?skillemp esco:hasSkill <%s>.
-                     ?employee default:function ?function.
-                     ?function default:startDate ?sDate.
-                     ?skillemp default:acquired ?aDate.
-                 OPTIONAL{
-                     ?function default:endDate ?date.
-                 } BIND(IF(BOUND(?date), ?date, %s ) AS ?eDate)
-             }
+    q = prefixes + """
+     SELECT distinct ?name ?sDate ?aDate ?eDate
+     WHERE{
+         GRAPH <http://mu.semte.ch/application>{
+             ?employee default:hasSkill ?skillemp.
+             ?employee foaf:name ?name.
+             ?skillemp esco:hasSkill <%s>.
+             ?employee default:function ?function.
+             ?function default:startDate ?sDate.
+             ?skillemp default:acquired ?aDate.
+         OPTIONAL{
+             ?function default:endDate ?date.
+         } BIND(IF(BOUND(?date), ?date, %s ) AS ?eDate)
+     }
          }""" % (skillURI, sparql_escape(today))
 
     data = helpers.query(q)
@@ -54,24 +38,18 @@ def get_skill_timeline():
     else:
         return jsonify({'data': []})
 
-
-def get_occupation_bubble():
-    pass
-
 @app.route('/skills/')
 def all_skills():
-    q = """ prefix esco: <http://data.europa.eu/esco/model#>
-            prefix skosxl: <http://www.w3.org/2008/05/skos-xl#>
-            prefix default: <http://example.org/MyCompany/>
-            SELECT distinct (?skill as ?skillURI) (str(?literal) as ?skillName) ?id
-            WHERE{
-                GRAPH <http://mu.semte.ch/application>{
-                    ?skill a esco:Skill.
-                    ?skill ^esco:hasSkill ?se.
-                    ?se a default:EmployeeSkill.
-                    ?skill skos:prefLabel ?literal.
-                    ?skill <http://mu.semte.ch/vocabularies/core/uuid> ?id.
-                }
+    q = prefixes + """
+    SELECT distinct (?skill as ?skillURI) (str(?literal) as ?skillName) ?id
+    WHERE{
+        GRAPH <http://mu.semte.ch/application>{
+            ?skill a esco:Skill.
+            ?skill ^esco:hasSkill ?se.
+            ?se a default:EmployeeSkill.
+            ?skill skos:prefLabel ?literal.
+            ?skill <http://mu.semte.ch/vocabularies/core/uuid> ?id.
+        }
             }"""
     res = helpers.query(q)
     data = res["results"]["bindings"]
@@ -84,15 +62,51 @@ def all_skills():
             "id": e["id"]["value"],
             "type": "skills"
         })
-    helpers.log("recent")
+    return jsonify(jobj)
+
+
+@app.route('/bubble/')
+def local_bubble():
+    date = request.args.get("date")
+    date = datetime.strptime(date, "%Y-%m-%d").date()
+
+    q = prefixes + """
+        SELECT (str(?skillName) as ?skillName) ?skillID (count(?skillemp) as ?count)
+        WHERE{
+          ?employee default:function ?function.
+          ?function esco:hasOccupation ?occ.
+          ?occ skos:prefLabel ?occN.
+
+          ?employee default:hasSkill ?skillemp.
+          ?skillemp esco:hasSkill ?skill.
+          ?skill mu:uuid ?skillID.
+          ?skill skos:prefLabel ?skillName.
+
+          ?skillemp default:acquired ?aDate.
+          ?function default:startDate ?sDate.
+
+          FILTER(?aDate < %s)
+          FILTER(%s > ?sDate)
+        } group by ?skillName ?skillID
+    """ % (sparql_escape(date), sparql_escape(date))
+    res = helpers.query(q)
+    data = res["results"]["bindings"]
+    jobj = {"data": []}
+    for e in data:
+        jobj["data"].append({
+            "attributes": {
+                "name": e["skillName"]["value"],
+                "count": e["count"]["value"]
+            },
+            "id": e["skillID"]["value"],
+            "type": "counts"
+        })
     return jsonify(jobj)
 
 
 @app.route('/occupations/')
 def get_all_occupations():
-    q = """ prefix esco: <http://data.europa.eu/esco/model#>
-            prefix skosxl: <http://www.w3.org/2008/05/skos-xl#>
-            prefix default: <http://example.org/MyCompany/>
+    q = prefixes + """
             SELECT distinct (?occ as ?occURI) (str(?literal) as ?occName) ?id
             WHERE{
                 GRAPH <http://mu.semte.ch/application>{
@@ -113,22 +127,14 @@ def get_all_occupations():
             "id": e["id"]["value"],
             "type": "occupations"
         })
-    helpers.log("recent")
     return jsonify(jobj)
 
 
 @app.route('/improvements/')
 def get_inprovement():
-    today = datetime.datetime.today().date()
+    today = datetime.today().date()
 
-    q = """
-        prefix esco: <http://data.europa.eu/esco/model#>
-        prefix skosxl: <http://www.w3.org/2008/05/skos-xl#>
-        prefix default: <http://example.org/MyCompany/>
-        prefix foaf: <http://xmlns.com/foaf/spec/>
-        prefix mu:  <http://mu.semte.ch/vocabularies/core/>
-        prefix skos: <http://www.w3.org/2004/02/skos/core#>
-
+    q = prefixes + """
         SELECT distinct ?skillName ?skillID ?empName ?empID ?occ ?sDate ?aDate ?eDate (str(?occN) as ?occupation)
         WHERE{
             ?employee default:hasSkill ?skillemp.
