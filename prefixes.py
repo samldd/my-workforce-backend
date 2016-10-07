8f08ac347957beb36ab30fc82568069de248d54e
sam <sam.landuydt>
2016-10-07 15:09:30 +0200

    prefixes added

---
+++ prefixes.py
@@ -0,0 +1,10 @@
+prefixes = """
+prefix foaf: <http://xmlns.com/foaf/spec/>
+prefix mu:  <http://mu.semte.ch/vocabularies/core/>
+prefix skos: <http://www.w3.org/2004/02/skos/core#>
+prefix default: <http://example.org/MyCompany/>
+prefix esco: <http://data.europa.eu/esco/model#>
+prefix skosxl: <http://www.w3.org/2008/05/skos-xl#>
+
+
+"""
